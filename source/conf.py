7de99325c0a1c9d4feb82b380f96a0224c7f059f
Kristijan Bakaric <kbakaric1@gmail.com>
2019-11-18 20:26:10 +0100

    Add interactive plots

--- source/conf.py
+++ source/conf.py
@@ -84,7 +84,7 @@ pygments_style = 'sphinx'
 import sphinx_press_theme
 import sphinx_rtd_theme
 
-html_theme = "press" #'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme" # "press" #'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
